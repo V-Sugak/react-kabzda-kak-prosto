71e2ff33c8bb07110e95b40c35559ea2a69e9949
V-Sugak <vicka.sugak2010@yandex.ru>
2022-02-12 16:35:03 +0300

    Select component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, KeyboardEvent, useEffect} from "react";
 import s from './Select.module.css'
 
 type ItemType = {
@@ -21,20 +21,45 @@ export const Select = (props: SelectPropsType) => {
     const selectedItem = props.items.find(i => i.value === props.value)
     const hoveredItem = props.items.find(i => i.value === hoveredItemValue)
 
+    useEffect(() => {
+        setHoveredItemValue(props.value)
+    }, [props.value])
+
     const toggleItems = () => setActive(!active)
     const onItemClick = (value: any) => {
         props.onChange(value)
         toggleItems()
     }
+    const onKeyUpHandler = (e: KeyboardEvent<HTMLDivElement>) => {
+        //    debugger
+        if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+            for (let i = 0; i < props.items.length; i++) {
+                if (props.items[i].value === hoveredItemValue) {
+                    const pretendentElement = e.key === "ArrowDown" ? props.items[i + 1] : props.items[i - 1]
+                    if (pretendentElement) {
+                        props.onChange(pretendentElement.value)
+                        return;
+                    }
+                }
+            }
+            if (!selectedItem) {
+                props.onChange(props.items[0].value)
+            }
+        }
+        if (e.key === 'Enter' || e.key === 'Escape') {
+            setActive(false)
+        }
+    }
 
-    return (<div className={s.select + ' ' + (active ? s.active : '')}>
+    return (<div className={s.select} onKeyUp={onKeyUpHandler} tabIndex={0}>
             <span className={s.main} onClick={toggleItems}>{selectedItem && selectedItem.title}</span>
 
-            {active && <div className={s.items}>
+            {active &&
+            <div className={s.items}>
                 {props.items.map(i => <div
                     className={s.item + ' ' + (hoveredItem === i ? s.selected : '')}
                     onMouseEnter={() => setHoveredItemValue(i.value)}
-                    key={props.value} onClick={() => {
+                    key={i.value} onClick={() => {
                     onItemClick(i.value)
                 }}>{i.title}</div>)}
             </div>}
