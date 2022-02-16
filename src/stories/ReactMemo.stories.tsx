8e527255a94d61acd5ab390efbfd49cd4aa1a53c
V-Sugak <vicka.sugak2010@yandex.ru>
2022-02-16 21:55:02 +0300

    React.memo

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+
+export default {
+    title: 'React.memo demo'
+}
+
+type CounterPropsType = { count: number }
+type UsersPropsType = { users: Array<string> }
+
+const Counter = (props: CounterPropsType) => {
+    return <div>{props.count}</div>
+}
+
+const UsersSecret = (props: UsersPropsType) => {
+    console.log('USERS')
+    return <div>
+        {props.users.map((u, i) => {
+            return <div key={i}>{u}</div>
+        })}
+    </div>
+}
+
+const Users = React.memo(UsersSecret)
+
+export const Example1 = () => {
+    const [count, setCount] = useState(0)
+    const [users, setUsers] = useState(['Kat', 'Ira', 'Ann'])
+
+    const addUser = () => {
+        return setUsers([...users, 'Sveta'])
+    }
+
+    return <>
+        <button onClick={addUser}>add user</button>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <Counter count={count}/>
+        <Users users={users}/>
+    </>
+}
